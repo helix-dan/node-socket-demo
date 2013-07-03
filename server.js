cc4dd892a4cc38b9a4ba68416e7640e751a82c58
helix-dan <lotus.bat@gmail.com>
2013-07-03 15:47:56 +0800

    add emit

--- server.js
+++ server.js
@@ -27,13 +27,12 @@ io.sockets.on('connection', function(socket){
 		socket.broadcast.emit('message', {text: 'a user disconnect, left ' + client_counter + ' users.', number: client_counter});
 	})
 
-	socket.emit('users', {number: client_counter})
+	socket.on('client_message', function(data){
+		console.dir('@@@@@@@' + data + '@@@@@@@');
+
+	})
+	socket.on
 
 	socket.emit('message', {text: 'you have connected!', number: client_counter})
 	socket.broadcast.emit('message', {text: 'a new user joined us!!, and we have ' + client_counter + ' users', number: client_counter})
 })
-
-
-// io.disable('heartbeats', function(socket){
-// 	console.log("llllllllllllllll")
-// });
