04c7bf1242cc043ce4844b4e815e0607115b2598
helix-dan <lotus.bat@gmail.com>
2013-07-04 18:19:11 +0800

    complete

--- server.js
+++ server.js
@@ -1,37 +1,20 @@
-var server = require('http').createServer(handler),
-		fs     = require('fs'),
-		io     = require('socket.io').listen(server);
-
-server.listen(3000);
-
-function handler (request, response) {
-	fs.readFile('./index.html', function(error, data){
-		if (error) {
-			response.writeHead(500);
-			return response.end('Server Error')
-		}
-
-		response.writeHead(200);
-		response.end(data, 'utf8');
-	});
-}
+var io = require('socket.io').listen(3300);
 
 var client_counter = 0
 
 io.sockets.on('connection', function(socket){
-	console.log("##############some one come in###############")
+	console.log("###### some one come in ######");
 	client_counter++;
 
 	socket.on('disconnect', function(){
 		client_counter--;
 		socket.broadcast.emit('message', {text: 'a user disconnect, left ' + client_counter + ' users.', number: client_counter});
-	})
+	});
 
 	socket.on('client_message', function(data){
 		console.dir('@@@@@@@' + data + '@@@@@@@');
-
-	})
-	socket.on
+		socket.emit('share_message', {text: 'you writen ' + data + ' just now'})
+	});
 
 	socket.emit('message', {text: 'you have connected!', number: client_counter})
 	socket.broadcast.emit('message', {text: 'a new user joined us!!, and we have ' + client_counter + ' users', number: client_counter})
